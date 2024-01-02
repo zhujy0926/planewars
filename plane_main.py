87c5052a8682dc7572e0476a5b31dfcdad46ef1a
zjy <zhujy0926@163.com>
2024-01-02 22:50:11 +0800

    update

--- plane_main.py
+++ plane_main.py
@@ -1,9 +1,7 @@
 # _*_ coding: utf-8 _*_
-import sys
+
 import pygame
 from plane_sprites import *
-
-#sys.setrecursionlimit(10**5)
 
 class PlaneGame(object):
     """飞机大战主游戏"""
@@ -16,11 +14,24 @@ class PlaneGame(object):
         self.clock = pygame.time.Clock()
         # 3. 调用私有方法，精灵和精灵组的创建
         self.__create_sprites()
+        # 4. 设置定时器事件 - 创建敌机  1s
+        pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
+        pygame.time.set_timer(HERO_FIRE_EVENT, 500)
+
 
     def __create_sprites(self):
         # 创建背景精灵和精灵组
-        bg1 = Background("./images/background.png")
-        self.back_group = pygame.sprite.Group(bg1)
+        bg1 = Background()
+        bg2 = Background(True)
+        self.back_group = pygame.sprite.Group(bg1, bg2)
+        # 创建敌机的精灵组
+        self.enemy_group = pygame.sprite.Group()
+
+        # 创建英雄的精灵和精灵组
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
+
+
 
 
     def start_game(self):
@@ -43,13 +54,57 @@ class PlaneGame(object):
             # 判断是否退出游戏
             if event.type == pygame.QUIT:
                 PlaneGame.__game_over()
+            elif event.type == CREATE_ENEMY_EVENT:
+                print("敌机出场...")
+                # 创建敌机精灵
+                enemy = Enemy()
+                # 将敌机精灵添加到敌机精灵组
+                self.enemy_group.add(enemy)
+            elif event.type == HERO_FIRE_EVENT:
+                self.hero.fire()
+            #elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            #    print("向右移动...")
+
+        # 使用键盘提供的方法获取键盘按键 - 按键元祖
+        keys_pressed = pygame.key.get_pressed()
+        # 判断元组中对应的按键索引值 1
+        if keys_pressed[pygame.K_RIGHT]:
+            #print("持续向右移动...")
+            self.hero.speed = 2
+        elif keys_pressed[pygame.K_LEFT]:
+            self.hero.speed = -2
+        else:
+            self.hero.speed = 0
+
 
     def __check_collide(self):
-        pass
+        # 1. 子弹摧毁敌机
+        pygame.sprite.groupcollide(self.hero.bullets, self.enemy_group, True, True)
+
+        # 2. 敌机撞毁英雄
+        enemies = pygame.sprite.spritecollide(self.hero, self.enemy_group, True)
+
+        # 判断列表是否有内容
+        if len(enemies) > 0:
+            # 让英雄牺牲
+            self.hero.kill()
+
+            # 结束游戏
+            PlaneGame.__game_over()
 
     def __update_sprites(self):
         self.back_group.update()
         self.back_group.draw(self.screen)
+
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen)
+
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
+
 
     @staticmethod
     def __game_over():
